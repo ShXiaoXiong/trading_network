395fb8010db5c60e615b6b8b69dfb88203e214c0
瀚 石 <shihan3610@163.com>
2020-01-17 14:17:26 +0800

    1月17日

--- data_restructure.py
+++ data_restructure.py
@@ -9,15 +9,18 @@ t=251#第一个预测值的位置
 closing_prices=[]
 inputs_list=[]#全部转换成列表
 
-for xx in range(734-251):#跺步次数
+
+for xx in range(734-251):#跺步次数，734-251=483
     new_input=[]
-    for record in data_list[t-250:t-1]:#往前推250个交易日数据，一共250行，250*7=1750个数据
+    for record in data_list[t-250:t]:#往前推250个交易日数据，一共250行，250*7=1750个数据，索引位置从1，到251
         
         b = record.split(',')#指定分隔符‘，’，对字符串进行切片，返回一个列表
+        b = [x.strip() for x in b if x.strip() != '']#不等于空就移除字符串头尾指定的字符（默认为空格或换行符）
         new_input+=b#列表拼接
 
-    inputs_list.append(new_input)
-    pass
+    inputs_list.append(new_input)#加到外列表中
+    t +=1
+
 
 with open('data_restructured.csv', 'w', newline='') as csvfile:
     writer  = csv.writer(csvfile)
--- trading_network.py
+++ trading_network.py
@@ -1,6 +1,7 @@
 import numpy #数组功能
 import scipy.special #激活函数
 import matplotlib.pyplot as plt #可视化
+import csv
 
 class Neuralnetworks:
     def __init__(self,inputnodes,hiddennodes,outputnodes,learningrate):
@@ -55,77 +56,124 @@ class Neuralnetworks:
         pass
 
 
-#按250个交易日往前推
 
-#构建神经网络实例
-#输入为7*250个交易日
-n=Neuralnetworks(1750,100,2,0.3)
+n=Neuralnetworks(1750,200,2,0.02)
 
-#训练并对比
-for record in range(484):#训练次数=735-251+1+1=485次，最后一组用于计算最后一次的target
+##输入为7*250个交易日，准备训练数据
+data_file=open('data_restructured.csv','r')#每个新行表示一个新的数据库行
+data_list=data_file.readlines()#转换为列表
+data_list = [x.strip() for x in data_list if x.strip() != '']#不等于空就移除字符串头尾指定的字符（默认为空格或换行符）
+data_list = [x.strip('"') for x in data_list if x.strip() != '']#不等于空就移除字符串头尾指定的字符（"）
+data_file.close()
+
+#准备target数据
+with open('train.csv','r') as csvfile:
+    reader = csv.reader(csvfile)
+    column1 = [row[2]for row in reader]
+closing_prices=column1[1:]#去除标签行，保持一致
+
+#计分板：对应的，净值额、两个矩阵
+final_scores=[]
+in_matrix=[]
+out_matrix=[]
+
+for e in range(100):#epochs
+
+    scoreboard=[]
+    accuracies=[]
+    actions=[]
+
+    t=251#第一个预测值的位置
+    cash=10000
+    equity=0
+
+    #训练并对比
+    for record in data_list:
+        all_values=record.split(',')    
+        inputs=numpy.asfarray(all_values) 
+        inputs=numpy.array(inputs,ndmin=2).T#传递列表，转换为二维数组，转置
+
+        ###计算过程
+        hidden_inputs=numpy.dot(n.ihw,inputs)#点乘
+        hidden_outputs=n.activation_function(hidden_inputs)#使用激活函数
+
+        final_inputs=numpy.dot(n.how,hidden_outputs)#点乘
+        final_outputs=n.activation_function(final_inputs)#使用激活函数
+
+
     
-    #每次都重新组成收入
-    inputs_list=[]#全部转换成列表。
-    for record in data_list[t-250:t-1]:#往前推250个交易日数据，一共250行，250*7=1750个数据
-        b = record.split(',')#指定分隔符‘，’，对字符串进行切片，返回一个列表
-        closing_prices += b[2]#列表中的第【2】，是closing price
-        inputs_list +=b#列表拼接
-
-    inputs=numpy.array(inputs_list,ndmin=2).T#传递列表，转换为二维数组，转置
-
-
-    #计算过程
-    hidden_inputs=numpy.dot(n.ihw,inputs)#点乘
-    hidden_outputs=n.activation_function(hidden_inputs)#使用激活函数
-
-    final_inputs=numpy.dot(n.how,hidden_outputs)#点乘
-    final_outputs=n.activation_function(final_inputs)#使用激活函数
-
-    
-    #对比
-    networks_label=numpy.argmax(final_outputs)#取出最大值对应的索引值
-
-    #输出结果，计算净值
-    
-    closing_0=numpy.array(data_list[t-1],ndmin=2)[3]
-    closing_1=numpy.array(data_list[t],ndmin=2)    
-    
-    net_value=10000/closing_0*closing_1
-
-
-    if networks_label=correct_label:#涨
-        correct_label=1
-    else:#跌
-        correct_label=0
-    
-   
-    #每轮有一个新的targets值
-    targets=numpy.zeros(2)+0.01 
-
-    if closing_1>=closing_0:#涨了，应当满仓
-        targets[1]=0.99
-    else:#跌了，应该空仓
-        targets[0]=0.99
-
-    #训练过程
-   
-    targets=numpy.array(targets,ndmin=2).T#传递列表，转换为二维数组
+        ###对比
+        networks_label=numpy.argmax(final_outputs)#取出最大值对应的索引值，0或1
+        actions.append(networks_label)
         
-    output_errors=targets-final_outputs#计算误差
+        closing_0=numpy.array(closing_prices[t],ndmin=2)
+        closing_1=numpy.array(closing_prices[t+1],ndmin=2)    
 
-    #隐藏层误差
-    hidden_errors=numpy.dot(n.how.T,output_errors)#点乘
-    #反向传递，更新how权重
-    n.how += n.lr * numpy.dot((output_errors * final_outputs* (1-final_outputs)),numpy.transpose(hidden_outputs))#点乘
-    #反向传递，更新ihw权重
-    n.ihw += n.lr * numpy.dot((hidden_errors * hidden_outputs* (1-hidden_outputs)),numpy.transpose(inputs))#点乘
-    pass
+        if closing_1>=closing_0:#涨了，应当满仓
+            correct_label=1
+        else:#跌了，应该空仓
+            correct_label=0
 
+        if networks_label==correct_label:
+            scoreboard.append(1)
+        else:
+            scoreboard.append(0)
 
-#最终结果可视化        
-import matplotlib.pyplot as plt
+        #记录结果2：预测正确率
+        accuracy= sum(scoreboard)/len(scoreboard)
+        accuracies.append(accuracy)
 
-y=accuracies
-x=range(len(accuracies))
-plt.plot(x,y,label='Frist line',linewidth=3,color='r',marker='o', markerfacecolor='blue',markersize=12) 
-plt.show()
+        #记录输出3：净值
+        if networks_label==0 and cash==0: #按前一个交易日价格卖出
+            cash = equity
+            equity=0
+        if networks_label==0 and cash!=0:#保持空仓
+            net_value += 0
+        if networks_label==1 and cash==0:#保持满仓
+            equity = equity*float(closing_prices[t+1])/float(closing_prices[t]) #重新赋值
+        if networks_label==1 and cash!=0:#按前一个交易日价格买入
+            equity=cash/float(closing_prices[t])*float(closing_prices[t+1])
+            cash=0
+
+        net_value=cash + equity
+        net_values.append(net_value)
+
+        #每轮有一个新的target值
+        targets=numpy.zeros(2)+0.01 
+
+        if closing_1>=closing_0:#涨了，应当满仓
+            targets[1]=0.99
+        else:#跌了，应该空仓
+            targets[0]=0.99
+
+        #训练过程
+        targets=numpy.array(targets,ndmin=2).T#传递列表，转换为二维数组
+        output_errors=targets-final_outputs#计算误差
+
+        #隐藏层误差
+        hidden_errors=numpy.dot(n.how.T,output_errors)#点乘
+        #反向传递，更新how权重
+        n.how += n.lr * numpy.dot((output_errors * final_outputs* (1-final_outputs)),numpy.transpose(hidden_outputs))#点乘
+        #反向传递，更新ihw权重
+        n.ihw += n.lr * numpy.dot((hidden_errors * hidden_outputs* (1-hidden_outputs)),numpy.transpose(inputs))#点乘
+    
+        t+=1
+    
+    #输出结果
+    print(net_value)   
+    final_scores.append(net_value)
+    out_matrix.append(n.how) 
+    in_matrix.append(n.ihw)
+
+#取分最高的索引，并输出
+suoying=final_scores.index(max(final_scores))
+
+with open('in_matrix.csv', 'w', newline='') as csvfile:
+    writer  = csv.writer(csvfile)
+    for row in in_matrix[suoying]:
+        writer.writerow(row)
+
+with open('out_matrix.csv', 'w', newline='') as csvfile:
+    writer  = csv.writer(csvfile)
+    for row in out_matrix[suoying]:
+        writer.writerow(row)
